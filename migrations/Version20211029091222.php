4a1d798a37db2937d99e8bbe76555a0499b1443a
Cindano <cindanojonathan@icloud.com>
2021-10-29 11:19:23 +0200

    fix migration

--- migrations/Version20211029091222.php
+++ migrations/Version20211029091222.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211028141518 extends AbstractMigration
+final class Version20211029091222 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,11 @@ final class Version20211028141518 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE annonce DROP FOREIGN KEY FK_F65593E5A76ED395');
+        $this->addSql('DROP INDEX IDX_F65593E5A76ED395 ON annonce');
+        $this->addSql('ALTER TABLE annonce CHANGE user_id eleveur_spa_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE annonce ADD CONSTRAINT FK_F65593E533062D77 FOREIGN KEY (eleveur_spa_id) REFERENCES elveurs_spa (id)');
+        $this->addSql('CREATE INDEX IDX_F65593E533062D77 ON annonce (eleveur_spa_id)');
         $this->addSql('ALTER TABLE photo DROP FOREIGN KEY FK_14B78418634DFEB');
         $this->addSql('DROP INDEX IDX_14B78418634DFEB ON photo');
         $this->addSql('ALTER TABLE photo CHANGE dog_id dogs_id INT DEFAULT NULL');
@@ -30,6 +35,11 @@ final class Version20211028141518 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE annonce DROP FOREIGN KEY FK_F65593E533062D77');
+        $this->addSql('DROP INDEX IDX_F65593E533062D77 ON annonce');
+        $this->addSql('ALTER TABLE annonce CHANGE eleveur_spa_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE annonce ADD CONSTRAINT FK_F65593E5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_F65593E5A76ED395 ON annonce (user_id)');
         $this->addSql('ALTER TABLE photo DROP FOREIGN KEY FK_14B78418D0AFB20A');
         $this->addSql('DROP INDEX IDX_14B78418D0AFB20A ON photo');
         $this->addSql('ALTER TABLE photo CHANGE dogs_id dog_id INT DEFAULT NULL');
