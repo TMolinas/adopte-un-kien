e4a0c73229a1b0c38ca534da4f9b23e6b1c83e51
mika <mickaelsalimi@gmail.com>
2021-11-03 16:32:17 +0100

    Ajout de l'event listener

---
+++ src/EventSubscriber/UserSubscribeSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserSubscribeSubscriber implements EventSubscriberInterface
+{
+    private UserPasswordHasherInterface $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this -> encoder = $encoder;
+    }
+
+    /**
+     * @param BeforeEntityPersistedEvent| BeforeEntityUpdatedEvent
+     */
+    public function onBeforeEntityPersistanceEvent( $event)
+    {
+        $user = $event->getEntityInstance();
+        if(!$user instanceof User){
+            return;
+        }
+
+        if (empty($user->getPlainPassword())){
+            return;
+        }
+        //encoder le password
+        $newPwd = $this ->encoder->hashPassword($user, $user->getPlainPassword());
+        $user->setPassword($newPwd);
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeForeEntityPersistedEvent::class => 'onBeForEntityPersistedEvent',
+            BeforeEntityUpdatedEvent::class => 'onBeForEntityPersistedEvent',
+
+        ];
+    }
+}
