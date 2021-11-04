cfd38d72c09d7955d49d25e1c138b48291da1dec
Teva Phauk <kilian.matis@gmail.com>
2021-11-04 16:15:53 +0100

    ajout du formulaire dog

---
+++ src/Form/DogType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dog;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nameOfDog')
+            ->add('breed')
+            ->add('lof')
+            ->add('description')
+            ->add('sociability')
+            ->add('canBeAdopted')
+            ->add('age')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Dog::class,
+        ]);
+    }
+}
